320d6a0a9f00a9be353c7ed2aa2f05a358d270d9
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-11-11 10:18:19 +0600

    review bg

--- src/Pages/Home/Reveiws/Review/Review.js
+++ src/Pages/Home/Reveiws/Review/Review.js
@@ -4,18 +4,31 @@ import React from 'react';
 const Review = ({ review }) => {
     const { name, img, title, rating, description } = review;
 
+    const reviewBg = {
+        color: '#C1C1C1',
+        background: `url("https://i.ibb.co/1sCF4PY/reviewbg.jpg")`,
+        backgroundPosition: 'center center',
+        backgroundSize: 'cover',
+        backgroundColor: 'rgba(21, 22, 24, 0.9)',
+        backgroundBlendMode: 'darken, luminosity'
+    }
+
     return (
-        <Paper sx={{ textAlign: 'center' }} elevation={0}>
+        <Paper sx={{ textAlign: 'center' }} style={reviewBg} elevation={0}>
             <img style={{ width: '50px', borderRadius: '25px' }} src={img} alt="" />
-            <h2>{name}</h2>
-            <Typography variant="caption" display="block" gutterBottom>
+            <Typography sx={{ color: 'white' }} variant="h5" display="block" gutterBottom>
+                {name}
+            </Typography>
+            <Typography sx={{ color: 'white' }} variant="caption" display="block" gutterBottom>
                 {title}
             </Typography>
             <Rating name="half-rating-read" defaultValue={rating} precision={0.5} readOnly />
-            <Typography variant="caption" display="block" gutterBottom>
+            <Typography sx={{ color: 'white' }} variant="caption" display="block" gutterBottom>
                 Review {rating} stars
             </Typography>
-            <p>{description}</p>
+            <Typography sx={{ color: 'white' }} variant="body1" display="block" gutterBottom>
+                {description}
+            </Typography>
         </Paper>
     );
 };
--- src/Pages/Home/Reveiws/Reviews.js
+++ src/Pages/Home/Reveiws/Reviews.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Carousel from 'react-material-ui-carousel'
 import { Container, Typography } from '@mui/material'
 import Review from './Review/Review';
+import { Box } from '@mui/system';
 
 const Reviews = (props) => {
     const [reviews, setReviews] = useState([]);
@@ -12,19 +13,30 @@ const Reviews = (props) => {
             .then(data => setReviews(data))
     }, [])
 
+    const reviewsBg = {
+        color: '#C1C1C1',
+        background: `url("https://i.ibb.co/1sCF4PY/reviewbg.jpg")`,
+        backgroundPosition: 'center center',
+        backgroundSize: 'cover',
+        backgroundColor: 'rgba(21, 22, 24, 0.9)',
+        backgroundBlendMode: 'darken, luminosity'
+    }
+
     return (
-        <Container sx={{ my: 5 }}>
-            <Typography sx={{ textAlign: 'center' }} variant="caption" display="block" gutterBottom>
-                What Our Happy Clients say about us
-            </Typography>
-            <Typography sx={{ textAlign: 'center', fontWeight: 600 }} variant="h4" display="block" gutterBottom>
-                Clients Reviews
-            </Typography>
-            <Carousel>
-                {
-                    reviews.map(review => <Review key={review._id} review={review}></Review>)
-                }
-            </Carousel>
+        <Container style={reviewsBg} sx={{ py: 5 }}>
+            <Box sx={{ textAlign: 'center' }}>
+                <Typography sx={{ color: 'white' }} variant="caption" display="block" gutterBottom>
+                    What Our Happy Clients say about us
+                </Typography>
+                <Typography sx={{ fontWeight: 600, color: 'white' }} variant="h4" display="block" gutterBottom>
+                    Clients Reviews
+                </Typography>
+                <Carousel>
+                    {
+                        reviews.map(review => <Review key={review._id} review={review}></Review>)
+                    }
+                </Carousel>
+            </Box>
         </Container>
     )
 }
